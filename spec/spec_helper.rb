6e0d34d896689e87afb3cb4eb68662e3bce475de
Rob Cobb <rwcobbjr@gmail.com>
2019-11-27 16:48:17 -0500

    update solution to work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ if ActiveRecord::Base.connection.migration_context.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
 end
 
+ActiveRecord::Base.logger = nil
+
 RSpec.configure do |config|
 
   config.include Rack::Test::Methods
